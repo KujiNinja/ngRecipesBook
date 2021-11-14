feb0fb0853a300f985f1c7eacf8674fe797bf680
Aleksey P <pravdin@my.com>
2021-11-14 17:17:24 +0300

    верстка рецептов

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import { Recipe } from '../recipes.model'
 })
 export class RecipeListComponent implements OnInit {
   recipes:Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simple desc', 'https://realfood.tesco.com/media/images/RFO-October2020-65809-Tesco-LetsCook-Oct20-65850-SpicedChickenGreenBeans1400x919-38f3e9b0-7241-49a3-83fe-fcc38d2c24be-0-1400x919.jpg')
+    new Recipe('A Test Recipe', 'This is simple desc', 'https://realfood.tesco.com/media/images/RFO-October2020-65809-Tesco-LetsCook-Oct20-65850-SpicedChickenGreenBeans1400x919-38f3e9b0-7241-49a3-83fe-fcc38d2c24be-0-1400x919.jpg'),
+    new Recipe('A Test Recipe 2', 'This is simple desc 2', 'https://realfood.tesco.com/media/images/RFO-October2020-65809-Tesco-LetsCook-Oct20-65850-SpicedChickenGreenBeans1400x919-38f3e9b0-7241-49a3-83fe-fcc38d2c24be-0-1400x919.jpg')
+
   ];
 
   constructor() { }
